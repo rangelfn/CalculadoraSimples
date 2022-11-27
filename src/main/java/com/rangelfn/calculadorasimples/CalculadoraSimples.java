83480f978bbc03369cbe5034641a29b32ba00fc3
Rangel Fernandes Nepomuceno <rangelnepomuceno@gmail.com>
2022-11-27 07:57:50 -0400

    inicial commit
    inicial commit

---
+++ src/main/java/com/rangelfn/calculadorasimples/CalculadoraSimples.java
@@ -0,0 +1,17 @@
+/*
+ *  COPYRIGHT © 2022 Rangel Nepomuceno
+ *  https://www.linkedin.com/in/rangelfn/
+ */
+
+package com.rangelfn.calculadorasimples;
+
+/**
+ *
+ * @author nepomuceno
+ */
+public class CalculadoraSimples {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
